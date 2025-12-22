3e47f68e8a3ede13d57dbe042dc5a97af292e582
Saurabh1Barasiya <saurabhbarasiya.sb@gmail.com>
2025-12-22 20:53:24 +0530

    apply dynamic class in a for loop.

---
+++ tut-3/src/app/app.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'tut-3';
+
+  users = [
+    { id: 1, name: 'Saurabh', role: 'Admin' },
+    { id: 2, name: 'Rahul', role: 'User' },
+    { id: 3, name: 'amit', role: 'User' },
+    { id: 4, name: 'peeyush', role: 'User' },
+    { id: 5, name: 'suhani', role: 'User' },
+    { id: 6, name: 'sohan', role: 'User' },
+    { id: 7, name: 'poja', role: 'User' },
+  ];
+}
